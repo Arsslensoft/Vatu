71ac470f3391cd5bdc1a60ba3dcccc39a8a24008
Arsslensoft <arsslens021@gmail.com>
2015-12-06 11:22:01 +0100

    Initial Commit
    Creation of projects VCC / Vasm

---
+++ VCC/VCC/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VCC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Copyright (c) 2015 Arsslensoft Research. All rights reserved");
+            Console.WriteLine("By Abd Al Moez Bouraoui and Arsslen Idadi");
+
+        }
+    }
+}
